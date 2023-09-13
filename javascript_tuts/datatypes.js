66dcd119d49734e2bea4e812c3e8915bcdfcc44b
BhesRajAcharya <117445666+BhesRajAcharya@users.noreply.github.com>
2023-09-13 06:09:07 +0000

    datatypes

---
+++ javascript_tuts/datatypes.js
@@ -0,0 +1,13 @@
+// String data type
+let name="bijaya"
+// number data type
+let age=22
+// boolean data type
+let isstudent=false
+// null data type
+let state=null
+// undefined data type
+let address
+console.log(typeof name)
+console.log(typeof(null))
+console.log(typeof(undefined))
