b0082974dcd7af75fcef4860e7b3582f55619f63
BhesRajAcharya <117445666+BhesRajAcharya@users.noreply.github.com>
2023-09-13 05:54:23 +0000

    changes

---
+++ javascript_tuts/variable.js
@@ -0,0 +1,13 @@
+const num=5578
+let name='bijaya'
+var password='bij_123'
+city='sunwal'
+let date;
+// num=456 //not allowed
+console.log(num)
+name='arjun'
+password='ar123'
+console.log(name);
+console.log(password);
+// alternative of print multiple variable
+console.table([num,name,password,date])
