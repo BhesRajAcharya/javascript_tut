420c75175e57626f509039a1810cf9890ec46d31
BhesRajAcharya <117445666+BhesRajAcharya@users.noreply.github.com>
2023-09-13 14:34:22 +0000

    typeconversion

---
+++ javascript_tuts/type_conversion.js
@@ -0,0 +1,47 @@
+let marks=40
+console.log(typeof(marks))
+let num="40"
+console.log(typeof(num))
+let score=null
+// converting null to number
+let nums=Number(num)
+console.log(typeof(nums))
+let scores=Number(score)
+console.log(typeof(scores))
+console.log(scores)
+let numbs="40abc"
+// conversion from string to number
+let nus=Number(numbs)
+console.log(typeof(nus))
+console.log(nus)
+// conversion from undefine to number
+let undefine=undefined
+let undef=Number(undefine)
+console.log(undef)
+console.log(typeof(undef))
+console.log(typeof(undefine))
+console.log(undefine)
+// conversion form boolean to number
+let isvoter=true
+let voter=Number(isvoter)
+console.log(typeof(voter))
+console.log(voter)
+// conversion from number to boolean
+let age=1
+let isvoters=Boolean(age)
+console.log(isvoters)
+let chars=""
+let ischars=Boolean(chars)
+console.log(ischars)
+let charas="bijaya"
+let ischaras=Boolean(charas)
+console.log(ischaras)
+// conversion from string to boolean
+let charnum="123"
+let ischarnum=Boolean(charnum)
+console.log(ischarnum)
+// conversion from number to string
+let somenumber=45
+let stringnum=String(somenumber)
+console.log(typeof(stringnum))
+console.log(stringnum)
