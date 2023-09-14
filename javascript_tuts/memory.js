02fa9ad3ebf6af866c21ef44a01b3a7372f9b440
BhesRajAcharya <117445666+BhesRajAcharya@users.noreply.github.com>
2023-09-14 12:45:25 +0000

    memory

---
+++ javascript_tuts/memory.js
@@ -0,0 +1,14 @@
+let fname='bijaya'
+let name= fname
+ name='udaya'
+ console.log(fname)
+ console.log(name)
+ let obj1={
+    firstname:'arjun',
+    lname:'karki',
+    address:'sunwal'
+ }
+ let obj2=obj1
+ obj2.firstname='raju'
+ console.log(obj1)
+ console.log(obj2)
